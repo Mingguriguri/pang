abe89e9738c62c2f4976c5fcfe763ac05a4db941
Mingguriguri <rlaalswjd-@naver.com>
2023-05-26 01:43:06 +0900

    add ITEM INPUT

--- capstone/pang/urls.py
+++ capstone/pang/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("show/", views.show_items, name="show_items"),
-    path("show/<int:category_id>:", views.detail, name="detail"),
+    path("show/<int:category_id>", views.detail, name="detail"),
     path("add_category/", views.add_category, name="add_category"),
+    path("add_item/<int:category_id>", views.add_item, name="add_item"),
 ]
--- capstone/pang/views.py
+++ capstone/pang/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Category, Item
 # input
-from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404, redirect
 
 def index(request):
     return HttpResponse("Hello, world. You're at the pang index.")
@@ -27,8 +27,21 @@ def detail(request, category_id):
     
     return render(request, 'pang/detail.html', context)
 
+
 @require_POST
 def add_category(request):
     category_name = request.POST['category_name']
     Category.objects.create(name=category_name)
     return redirect('show_items')
+
+@require_POST
+def add_item(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+   # item_name = request.POST['item_name'] Error 
+   #대부분 메서드를 사전(Dictionary) 또는 리스트(List)처럼 접근하려고 할 때 발생합니다.
+    item_name = request.POST.get('item_name')
+    
+    if item_name:  # Check if item_name is not None
+        Item.objects.create(contents=item_name, category=category)
+
+    return redirect('detail', category_id=category_id)

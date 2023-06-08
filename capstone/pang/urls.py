572db4e95cad120c6b9d9558ae9730bb68e89651
Mingguriguri <rlaalswjd-@naver.com>
2023-06-09 04:00:57 +0900

    font

--- capstone/pang/urls.py
+++ capstone/pang/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("show/", views.show, name="show"),
-    #path("show/<int:category_id>", views.detail, name="detail"),
+]
+
+
+
+#path("show/<int:category_id>", views.detail, name="detail"),
     #path("add_category/", views.add_category, name="add_category"),
     #path("add_item/<int:category_id>", views.add_item, name="add_item"),
     #path('delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
-
-]
---
+++ capstone/주먹구구_v2/click.js
@@ -0,0 +1,7 @@
+$(function (){
+	$("#btn_toggle").click(function (){
+        let hi = document.querySelector("test");
+        hi.style.display = "block";
+  	$("#Toggle").toggle();
+  });
+});
